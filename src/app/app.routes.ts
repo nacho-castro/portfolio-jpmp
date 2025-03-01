61debc7107b98ee22f3f0f21b554408612402ae5
nacho-castro <ignaciocastroplanas@gmail.com>
2025-03-01 12:42:42 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ObrasComponent } from './components/obras/obras.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TallerComponent } from './components/taller/taller.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'inicio', component: InicioComponent},
+    {path: '', component: InicioComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'obras', component: ObrasComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'taller', component: TallerComponent},
+];
---
+++ src/app/components/obras/obras.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obras',
+  standalone: true,
+  imports: [],
+  templateUrl: './obras.component.html',
+  styleUrl: './obras.component.css'
+})
+export class ObrasComponent {
+
+}
